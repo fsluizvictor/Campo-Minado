e6f3e8a94ed4d3fca739ba47134d32677308da22
fsluizvictor <fsluizvictor@gmail.com>
2020-05-14 17:30:51 -0300

    desenvolvimento dos campos

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Params from './src/params'
+import params from './src/params';
+import Field from './src/components/Field'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Iniciando o Mines!</Text>
+      <Text>Tamanho da grade:
+        {params.getRowsAmount()} x {params.getCollumnsAmount()}
+      </Text>
+      <Field />
+      <Field opened />
+      <Field opened nearMines={1} />
+      <Field opened nearMines={2} />
+      <Field opened nearMines={3} />
+      <Field opened nearMines={6} />
+      <Field mined />
+      <Field mined opened />
+      <Field mined opened exploded />
+      <Field flagged />
+      <Field flagged opened />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -16,4 +33,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
